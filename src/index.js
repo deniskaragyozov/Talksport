455da91a185fc2d8f77caa4c79b9f4435709f859
Denis Karagyozov <jsdk9926@gmail.com>
2025-03-03 11:55:57 +0200

    Install and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.use(express.static('src/public'));
+
 app.get('/', (req, res) => {
-    res.send('Express setup');
 });
 
 app.listen(3030, () => console.log('Server is listening on http://localhost:3030...'));
