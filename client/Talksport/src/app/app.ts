22903e447d2d9b4cea44fbb7b4ccbaeb97d9b0d7
Denis Karagyozov <jsdk9926@gmail.com>
2025-08-05 17:08:00 +0300

    Add Home feature and route

--- client/Talksport/src/app/app.ts
+++ client/Talksport/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header, Footer } from "./shared/components";
+import { Home } from "./features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
