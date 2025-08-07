345ce682e183e0640a0c2fffb386295963702529
Denis Karagyozov <jsdk9926@gmail.com>
2025-08-07 03:11:31 +0300

    Implement reactive login form with validation

--- client/Talksport/src/app/features/login/login.ts
+++ client/Talksport/src/app/features/login/login.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/)]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    })
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.loginForm.get('password');
+  }
+
+  get isEmailValid(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+  }
+
+  get isPasswordValid(): boolean {
+    return this.password?.invalid && (this.password?.dirty || this.password?.touched) || false;
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required!';
+    }
+
+    if (this.email?.errors?.['pattern']) {
+      return 'Invalid email!';
+    }
+
+    return '';
+  }
+
+   get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required!';
+    }
+
+    if (this.password?.errors?.['minlength']) {
+      return 'Password should be at least 5 characters long!';
+    }
+
+    return '';
+  }
+
+  onSubmit(): void {
+    if(this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.authService.login(email, password).subscribe({
+        next: () => {
+            this.router.navigate(['/home']);
+          },
+          error: (err) => {
+            console.log('An error occured in login', err)
+            this.markFormGroupTouched();
+          }
+      })
+    }
+  }
+
+   private markFormGroupTouched(): void {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      if(control instanceof FormGroup) {
+        Object.keys(control.controls).forEach(nestedKey => {
+          const nestedControl = control.get(nestedKey);
+          nestedControl?.markAllAsTouched();
+        })
+      }else{
+        control?.markAsTouched();
+      }
+    })
+  }
 }
