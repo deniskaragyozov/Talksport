845d5d473f1bc8de1fe6eb73db873f15dac4388c
Denis Karagyozov <jsdk9926@gmail.com>
2025-08-09 04:32:15 +0300

    Add routerLink to latest post view more button

--- client/Talksport/src/app/features/home/home.ts
+++ client/Talksport/src/app/features/home/home.ts
@@ -3,10 +3,11 @@ import { Article } from '../../models';
 import { Observable } from 'rxjs';
 import { ArticlesService } from '../../core/services';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
