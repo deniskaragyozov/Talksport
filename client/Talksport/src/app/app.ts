e6830e1b86723ed7a220c27556dd9ad7e8f9e3d5
Denis Karagyozov <jsdk9926@gmail.com>
2025-08-10 04:28:13 +0300

    Add error notification component to app template

--- client/Talksport/src/app/app.ts
+++ client/Talksport/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header, Footer } from "./shared/components";
 import { Home } from "./features/home/home";
+import { ErrorNotification } from './shared/components/error-notification/error-notification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ErrorNotification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
